816e0b9350dad8ab93c14581a27d25e3a4a17544
Conor Corkery <ccorkery97@gmil.com>
2020-05-09 18:17:03 -0400

    got all the elements for the html and css aligned just right

---
+++ main.js
@@ -0,0 +1,24 @@
+const slideshow = document.querySelector('.slideshow')
+const slides = Array.from(slideshow.querySelectorAll('.slide'))
+const slideCount = slides.length
+
+const slideWidth = slides[0].getBoundingClientRect().width
+
+
+rightButton = document.querySelector('.right')
+rightButton.addEventListener('click', transitionSlide)
+
+let currentSlide = 1;
+let currentPosition =0;
+
+let transformOption
+function transitionSlide() {
+  if (currentSlide < slideCount) {
+    currentPosition -= slideWidth
+    transformOption = `translateX(${currentPosition}px)`
+    slideshow.style.transform = transformOption
+    currentSlide++
+  } 
+}
+
+
